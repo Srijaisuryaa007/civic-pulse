89310655a1a928c97e3765e04da9bd06dd9411bf
jaisuryaa <dropshipin85@gmail.com>
2026-06-28 12:45:33 +0530

    Change bucket domain back to .appspot.com for Admin SDK

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -199,7 +199,7 @@ try {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      storageBucket: `${serviceAccount.project_id}.firebasestorage.app`
+      storageBucket: `${serviceAccount.project_id}.appspot.com`
     });
   }
   db = admin.firestore();
